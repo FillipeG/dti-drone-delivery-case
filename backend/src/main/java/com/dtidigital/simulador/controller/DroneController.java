b99dc7d884baacc6e060e9bafd7b181e5b0b57a1
Fillipe Gabriel <fillipe.gabriel00@hotmail.com>
2026-07-24 14:08:00 -0300

    feat: cria controllers de Drone e Pedido

---
+++ backend/src/main/java/com/dtidigital/simulador/controller/DroneController.java
@@ -0,0 +1,41 @@
+package com.dtidigital.simulador.controller;
+
+import com.dtidigital.simulador.model.Drone;
+import com.dtidigital.simulador.service.DroneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drones")
+@RequiredArgsConstructor
+
+public class DroneController {
+
+    private final DroneService droneService;
+
+    @PostMapping
+    public ResponseEntity<Drone> cadastrar(@RequestBody Drone drone){
+        Drone novoDrone = droneService.cadastrarDrone(drone);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoDrone);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Drone>> listarTodos(){
+        return ResponseEntity.ok(droneService.listarTodos());
+    }
+
+    @GetMapping("/disponiveis")
+    public ResponseEntity<List<Drone>> listarDisponiveis(){
+        return ResponseEntity.ok(droneService.listarDisponiveis());
+    }
+
+    @GetMapping("/em-uso")
+    public ResponseEntity<List<Drone>> listarEmUso(){
+        return ResponseEntity.ok(droneService.listarEmUso());
+    }
+    
+}
---
+++ backend/src/main/java/com/dtidigital/simulador/controller/PedidoController.java
@@ -0,0 +1,34 @@
+package com.dtidigital.simulador.controller;
+
+import com.dtidigital.simulador.dto.PedidoDTO;
+import com.dtidigital.simulador.model.Pedido;
+import com.dtidigital.simulador.service.PedidoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pedidos")
+@RequiredArgsConstructor
+
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody @Valid PedidoDTO dto){
+        Pedido novoPedido = pedidoService.criarPedido(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPedido);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pedido>> listarTodos(){
+        return ResponseEntity.ok(pedidoService.listarTodos());
+    }
+    
+}
