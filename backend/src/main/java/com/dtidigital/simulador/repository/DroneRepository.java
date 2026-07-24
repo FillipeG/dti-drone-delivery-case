c3d7113d3f0345842437ce52e0bde5cc24e7e4ff
Fillipe Gabriel <fillipe.gabriel00@hotmail.com>
2026-07-24 12:12:40 -0300

    feat: cria interfaces PedidoRepository e DroneRepository

---
+++ backend/src/main/java/com/dtidigital/simulador/repository/DroneRepository.java
@@ -0,0 +1,10 @@
+package com.dtidigital.simulador.repository;
+
+import com.dtidigital.simulador.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, String> {
+    
+}
